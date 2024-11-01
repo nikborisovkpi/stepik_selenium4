ecbd4d311abc2fb509d2621513cbab045c5e65f3
nikborisovkpi <nb02072004@gmail.com>
2024-11-01 04:24:36 +0200

    product page files added

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,10 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators ():
+class LoginPageLocators():
     LOGIN_URL = ()
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
