a7f414dae5b1e7a927c28a2607a042b5b3374517
nikborisovkpi <nb02072004@gmail.com>
2024-11-05 16:03:49 +0200

    test_product_page.py modified

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,20 @@
 from .pages.product_page import ProductPage
+import pytest
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# def test_guest_can_add_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.check_price_at_all()
+
+
+@pytest.mark.parametrize('index', ["offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                                   pytest.param("offer7", marks=pytest.mark.xfail), ])
+def test_guest_can_add_product_to_basket(browser, index):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={index}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
